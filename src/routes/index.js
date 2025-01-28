ecfc94c0224ad91c47d3e91c878d3d5b9cffc38a
Ed Angelis <ed4ngelis@gmail.com>
2025-01-28 16:41:28 -0300

    started

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,7 @@
 import { Router } from "express";
-import questionsRoute from "./questions.route.js";
-import examRoutes from "./exam.route.js";
 import userRoutes from "./user.route.js";
 
 const router = Router();
-
-router.use("/questions", questionsRoute);
-router.use("/exams", examRoutes);
 router.use("/users", userRoutes);
 
 export default router;
